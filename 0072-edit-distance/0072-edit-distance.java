45be7228bcb142452cad5a65b9f3b50ec7646ce6
Shubhneet Garg <shubhneet.garg1@gmail.com>
2026-09-10 11:55:03 +0530

    Time: 5 ms (65.53%), Space: 47.3 MB (35.16%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDistance(String S1, String S2) {
+        int n = S1.length();
+        int m = S2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (S1.charAt(i - 1) == S2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
